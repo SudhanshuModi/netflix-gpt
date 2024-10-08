f5cd8b2f645b17cd7d9f4c5a4557fa4855444166
Sudhanshu Modi <sudhanshu565868@gmail.com>
2024-10-08 09:00:23 +0530

    img css re-configured

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
       <Header />
       <div className="absolute">
         <img
-          className="h-screen w-screen object-cover"
+          className=""
           src="https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg"
           alt="netfix-background"
         />
